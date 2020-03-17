b2cdf0820c2bb6251abd40a9785f1b9c6aeecab4
Hui Kang <rocksnow1942@gmail.com>
2020-03-17 11:22:19 -0700

    a

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="huik-module", # Replace with your own username this is for PIP install, not the real python package name. the real name will be example_pkg
+    version="0.0.1",
+    author="Hui Kang",
+    author_email="rocksnow1942@gmail.com",
+    description="Modules for my scripts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(), # or can manually enter.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
