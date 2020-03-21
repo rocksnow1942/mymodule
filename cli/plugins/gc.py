0ad32dbb2620fa077aea26332cb6e6ee7638242c
Hui Kang <rocksnow1942@gmail.com>
2020-03-21 12:13:48 -0700

    ON: Sat Mar 21 12:13:47 2020, FROM: hui-mbp.lan

--- cli/plugins/gc.py
+++ cli/plugins/gc.py
@@ -1,5 +1,8 @@
 import click
-
+import subprocess as sub
+from datetime import datetime as date
+import socket 
+from cli.utils import TableDisplay
 
 @click.command()
 @click.argument("comment",nargs=-1,)
@@ -8,13 +11,14 @@ def cli(comment):
     git pull, add . , commit -m 'comment', git push
     4 step in one.
     """
-    import subprocess as sub
-    from datetime import datetime as date
-    import socket 
     msg = " ".join(comment)
     click.echo('\nGit pull')
     sub.run('git pull',shell=True,) 
     if not msg:
         msg = click.prompt("Enter a comment",show_default=True,
-            default=f"Commit on {date.now().strftime('%c')}, From {socket.gethostname()}")
+            default=f"ON: {date.now().strftime('%c')}, FROM: {socket.gethostname()}")
     p = sub.run(f'git add . \n git commit -m "{msg}" \n git push',shell=True) 
+
+    result = sub.run('git config --get remote.origin.url',stdout=sub.PIPE,encoding='utf-8',shell=True).stdout
+    td = TableDisplay()
+    click.echo(td(text=f"Commited to [{result.strip()}]"))
