c230e4df457f100ecd7f4f98fef159758f5c31db
rocksnow1942 <rocksnow1942@gmail.com>
2020-03-31 18:16:21 -0700

    ON: Tue Mar 31 18:16:17 2020, FROM: mini

--- cli/plugins/cc.py
+++ cli/plugins/cc.py
@@ -7,22 +7,32 @@ from datetime import datetime
 from pathlib import PurePath
 import shutil
 
-
 @click.command()
 @click.option('--folder','-f',default=None,type=click.Path(exists=False, file_okay=False,resolve_path=True),help="Provide folder to clean.")
 @click.option('--pattern','-p',default=None, help = "Use custom regex pattern to delete.")
-def cli(folder,pattern):
+@click.option('--reverse','-r',is_flag=True,default=False,help='Reverse the deletion, move files and folders back.')
+def cli(folder,pattern,reverse):
     """
     Clean files from a folder using regex pattern search.\n
     Default using icloud sync duplicate pattern / [2-9]$/ .
     """
     td=TableDisplay()
-    folder = folder or click.prompt(td(text="Enter [path/to/folder] you want to clean:")+'\n', default=os.getcwd())
+    op = 'reverse' if reverse else 'clean'
+    folder = folder or click.prompt(td(text=f"Enter [path/to/folder] you want to {op}:")+'\n', default=os.getcwd())
+    if reverse:
+        try:
+            click.echo(td(text=f'Reverse cleaning on folder [{folder}] ...'))
+            reverse_deletion(folder)
+            click.echo(td(title=">>> <g>SUCCESS</g> <<<",
+                          text=f'Reversed cleaning on folder [{folder}]'))
+        except Exception as e:
+            click.echo(td(title=">>> <r>ERROR</r> <<<",text=f"Error: <r>{e}</r>"))
+        return 
     pattern = pattern or click.prompt(td(text="Enter regex pattern:")+'\n', default=" [2-9]$") 
     try:
         pattern = re.compile(pattern)
     except re.error as e:
-        click.echo(td(text=f"Regex compile error: <r>{e}</r>"))
+        click.echo(td(title=">>> <r>ERROR</r> <<<",text=f"Regex compile error: <r>{e}</r>"))
         return 
     if os.path.isdir(folder):       
         click.echo(td(text=f'Cleaning folder [{folder}] ...'))
@@ -31,6 +41,27 @@ def cli(folder,pattern):
             text=f'Cleaning Done.\n{{{fileCount}}} files\n{{{folderCount}}} folders\nMoved to [{save}].'))
     else:
         click.echo(td(text=f"Path <r>{folder}</r> is invalid."))
+
+def reverse_deletion(path):
+    path = PurePath(path)
+    tempfolder = path / '!DELETE SCRIPT TEMP FOLDER'
+    filepath = tempfolder / 'Deleted files'
+    folderpath = tempfolder / 'Deleted folders'
+    f = open(tempfolder/'!Deletion Log.txt', 'r')
+    def move(f,t,tag):
+        if os.path.exists(f):
+            shutil.move(f,t) 
+            return
+        click.echo(f'Miss [{tag}] <{filepath/p}>')
+
+    for line in f.readlines():
+        p = line.strip()[15:-1]
+        if line.startswith("Move [ FILE ]"):
+            move(filepath/p, path/p,' FILE ')
+        elif line.startswith("Move [FOLDER]"):
+            move(folderpath/p, path/p,'FOLDER')
+    f.close()
+   
 
 def delete_by_pattern(path,pattern):
     regex = pattern.pattern
@@ -47,22 +78,23 @@ def delete_by_pattern(path,pattern):
     for root, dirs, files in os.walk(path):
         if str(tempfolder) in root:  # avoid looking into temp path.
             continue
+        relative = PurePath(root).relative_to(path)
         if pattern.search(root):  # if the root folder is matched.
-            relative = PurePath(root).relative_to(path)
             click.echo(f'Move [FOLDER] <{relative}>')
-            f.write(f'Move [FOLDER] <{root}>\n')
+            f.write(f'Move [FOLDER] <{relative}>\n')
             shutil.move(root, folderpath / relative)
             movedFolders += 1
             continue
         for file in files:
             if pattern.search(file):
                 click.echo(f'Move [ FILE ] <{file}>')
-                f.write(f'Move [ FILE ] <{os.path.join(root, file)}>\n')
-                os.rename(os.path.join(root, file), filepath/file)
+                f.write(f'Move [ FILE ] <{os.path.join(relative, file)}>\n')
+                os.makedirs(filepath / relative, exist_ok=True)
+                shutil.move(os.path.join(root, file), filepath/relative/file)
                 movedFiles += 1 #.append(oas.path.join(root, file))
 
     f.write('='*50+'\n')
     f.write(
-        f'Moved {movedFiles} files, {movedFolders} folders, finished on {datetime.now().strftime("%Y/%m/%d %H:%M")}\n\n')
+        f'Cleaned {movedFiles} files, {movedFolders} folders, finished on {datetime.now().strftime("%Y/%m/%d %H:%M")}\n\n')
     f.close()
     return tempfolder, movedFiles, movedFolders
