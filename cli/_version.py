7012827e30e2f67f54fd343cc4324e69e1c07e5b
rocksnow1942 <rocksnow1942@gmail.com>
2020-05-12 12:00:14 -0700

    ON: Tue May 12 11:58:33 2020, FROM: mini

--- cli/_version.py
+++ cli/_version.py
@@ -1,5 +1,6 @@
 """
 Change log:
+5/12/2020 added alignment module to file.
 5/6/2020 added RNA structure and Vienna RNA warpping.
 """
-__version__="0.1.8"
+__version__="0.1.9"
--- mymodule/__init__.py
+++ mymodule/__init__.py
@@ -10,9 +10,11 @@
 - mkdirs: function to make folder.
 - revcomp, lev_distance for Sequences
 - ft, FT_Decorator: time a function.
+- Alignment: multi sequence alignment. 
 """
 from .rotate_logger import FileLogger
 from .mypath import mkdirs
-from .seq import revcomp, lev_distance
+from .seq import revcomp
+from .align import lev_distance, Alignment
 from .tool import ft, ft_decorator,FT_Decorator,poolMap, mprint, MyPrint
-# from . import tool
+
